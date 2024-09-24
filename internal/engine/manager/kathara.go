650f53db5c267f5bc2a5d1e3b20091c5b16d3bdb
Yigit Hakverdi <hakverdiyigit@gmail.com>
2024-09-24 19:41:27 +0300

    few functions added to kathara manager

--- internal/engine/manager/kathara.go
+++ internal/engine/manager/kathara.go
@@ -1,9 +1,23 @@
 package manager
 
-func start_kathara() error {
-	return nil
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
+)
+
+func StartKathara() {
+	cmd := exec.Command("kathara", "lstart")
+	err := cmd.Run()
+	var out bytes.Buffer
+
+	cmd.Stdout = &out
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("Kathara started: %s", out.String())
 }
 
-func stop_kathara() error {
+func StopKathara() error {
 	return nil
 }
--- internal/engine/profiler.go
+++ internal/engine/profiler.go
@@ -1,13 +1,1 @@
 package engine
-
-func collect_cpu_usage() error {
-	return nil
-}
-
-func collect_memory_usage() error {
-	return nil
-}
-
-func collect_network_usage() error {
-	return nil
-}
