2387d78a17aa42f621e941a44623d65275fdb828
Yigit <hakverdiyigit@gmail.com>
2024-10-22 18:46:33 +0300

    Replace native graph implementation with github.com/dominikbraun/graph since it provides useful builtin implementations of bunch of important graph algos

--- cmd/main.go
+++ cmd/main.go
@@ -1,31 +1,64 @@
 package main
 
 import (
-	"log"
-  "fmt"
-	"rstk/internal/engine"
+	// "log"
+	"fmt"
+	// "os"
+	// "rstk/internal/engine"
+  "rstk/internal/parser"
+  "rstk/internal/graph"
+	// "github.com/dominikbraun/graph"
+	// "github.com/dominikbraun/graph/draw"
 )
 
 func main() {
 	// Initialize the graph from AS relationships
-	g := engine.InitializeGraph("data/serial-2/20151201.as-rel2.txt", []string{"#"})
+	// g := engine.InitializeGraph("data/serial-2/20151201.as-rel2.txt", []string{"#"})
+  
+  asNumber := 393809
 
-  // fmt.Printf("%v", g) 
-	// Initialize the simulation configuration
-	simulationConfig := engine.InitializeSimulationConfig()
+  parser := parser.Parser {
+    AsRelFilePath: "data/serial-2/20151201.as-rel2.txt",
+    BlacklistTokens: []string{"#"},
+  }
+  parser.ParseFile()
+  g := graph.PopulateGraph(parser.AsRelationships)
+  
+  providers, _ :=  graph.GetProviders(g, asNumber)
+  fmt.Printf("%v", providers)
+  
+  customer, _ :=  graph.GetCustomers(g, asNumber)
+  fmt.Printf("\n%v", customer)
+
+	//  adjMap, err := g.AdjacencyMap()
+	//
+	//  if err != nil {
+	//    panic(err)
+	//  }
+	//
+	//  if neighbours, exists := adjMap[1]; exists {
+	//    for _, neighbour := range neighbours {
+	//      fmt.Println(neighbour)
+	//    }
+	//  } else {
+	// 	fmt.Println("Vertex 1 not found.")
+	// }
+ 
+  // file, _ := os.Create("./mygraph.gv")
+  // _ = draw.DOT(gTemp, file)
+  // Initialize the simulation configuration
+	// simulationConfig := engine.InitializeSimulationConfig()
 
   // Set up simulation directory and config file
-	err := engine.SetupSimulationDirectory(&simulationConfig)
-	if err != nil {
-		log.Fatalf("Setup failed: %v", err)
-	}
- 
+	// err := engine.SetupSimulationDirectory(&simulationConfig)
+	// if err != nil {
+	// 	log.Fatalf("Setup failed: %v", err)
+	// }
+	
 	// Generate the topology for the simulation
-	asNumber := 1
-	topology := engine.GenerateTopology(asNumber, g, simulationConfig.Topology)
-  fmt.Printf("%v", topology)
-
-
+	// asNumber := 1
+	// topology := engine.GenerateTopology(asNumber, g, simulationConfig.Topology)
+	//  fmt.Printf("%v", topology)
 
 	// Generate and handle collision domains
 	// err = engine.GenerateCollisionDomains(simulationConfig.KatharaConfigPath, topology)
